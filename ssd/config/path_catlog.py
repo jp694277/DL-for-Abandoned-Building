9b43fd2b11d90006897eeb050aa223aed13318a9
Debbie Wang (Ziwen) <jp694277@gmail.com>
2021-06-14 13:52:36 +0800

    Add files via upload

---
+++ ssd/config/path_catlog.py
@@ -0,0 +1,104 @@
+import os
+
+
+class DatasetCatalog:
+    DATA_DIR = 'datasets'
+    DATASETS = {
+        'voc_2007_train': {
+            "data_dir": "VOC2007",
+            "split": "train"
+        },
+        'voc_2007_val': {
+            "data_dir": "VOC2007",
+            "split": "val"
+        },
+        'voc_2007_trainval': {
+            "data_dir": "VOC2007",
+            "split": "trainval"
+        },
+        'voc_2007_test': {
+            "data_dir": "VOC2007",
+            "split": "test"
+        },
+        'voc_2012_train': {
+            "data_dir": "VOC2012",
+            "split": "train"
+        },
+        'voc_2012_val': {
+            "data_dir": "VOC2012",
+            "split": "val"
+        },
+        'voc_2012_trainval': {
+            "data_dir": "VOC2012",
+            "split": "trainval"
+        },
+        'voc_2012_test': {
+            "data_dir": "VOC2012",
+            "split": "test"
+        },
+        'coco_2014_valminusminival': {
+            "data_dir": "val2014",
+            "ann_file": "annotations/instances_valminusminival2014.json"
+        },
+        'coco_2014_minival': {
+            "data_dir": "val2014",
+            "ann_file": "annotations/instances_minival2014.json"
+        },
+        'coco_2014_train': {
+            "data_dir": "train2014",
+            "ann_file": "annotations/instances_train2014.json"
+        },
+        'coco_2014_val': {
+            "data_dir": "val2014",
+            "ann_file": "annotations/instances_val2014.json"
+        },
+        'abandon_buildings': {
+            'data_dir': 'dataset/abandon_buildings',
+            'ann_file': 'annotations/abandon_buildings.xlsx'
+        },
+        'abandon_buildings_test': {
+            'data_dir': 'dataset/abandon_buildings_test',
+            'ann_file': 'annotations/abandon_buildings.xlsx'
+        },
+        'abbd': {
+            'data_dir': 'dataset/new_abbd',
+            'ann_file': 'annotations/new_abbd_gt_xxyy.xlsx'
+        },
+        'abbd_test': {
+            'data_dir': 'dataset/abbd_test',
+            'ann_file': 'annotations/new_abbd_gt_xxyy.xlsx'
+        }
+    }
+
+    @staticmethod
+    def get(name):
+        if "voc" in name:
+            voc_root = DatasetCatalog.DATA_DIR
+            if 'VOC_ROOT' in os.environ:
+                voc_root = os.environ['VOC_ROOT']
+
+            attrs = DatasetCatalog.DATASETS[name]
+            args = dict(
+                data_dir=os.path.join(voc_root, attrs["data_dir"]),
+                split=attrs["split"],
+            )
+            return dict(factory="VOCDataset", args=args)
+        elif "coco" in name:
+            coco_root = DatasetCatalog.DATA_DIR
+            if 'COCO_ROOT' in os.environ:
+                coco_root = os.environ['COCO_ROOT']
+
+            attrs = DatasetCatalog.DATASETS[name]
+            args = dict(
+                data_dir=os.path.join(coco_root, attrs["data_dir"]),
+                ann_file=os.path.join(coco_root, attrs["ann_file"]),
+            )
+            return dict(factory="COCODataset", args=args)
+        elif 'abandon_buildings' in name:
+            attrs = DatasetCatalog.DATASETS[name]
+            return dict(factory="MyDataset", args=attrs)
+        elif 'abbd' in name:
+            attrs = DatasetCatalog.DATASETS[name]
+            return dict(factory="MyDataset", args=attrs)
+
+        raise RuntimeError("Dataset not available: {}".format(name))
